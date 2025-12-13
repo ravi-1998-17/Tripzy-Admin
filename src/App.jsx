8826d666b450cc9e5cb0a0b3cd4b79be013ae7b7
Ravi Suthar <ravisuthar1711@gmail.com>
2025-12-13 16:46:29 +0530

    feat(routes): add protected admin routes for products and orders

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import { Route, Routes } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import PrivateAdminRoute from "./components/PrivateAdminRoute";
 import Dashboard from "./pages/Dashboard";
+import ProductsList from "./pages/ProductsList";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="/admin-login" element={<AdminLogin />}/>
-      <Route path="dashboard" element={<PrivateAdminRoute>
-        <Dashboard />
-      </PrivateAdminRoute> }/>
-    </Routes>
-      
+      <Routes>
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route
+          path="dashboard"
+          element={
+            <PrivateAdminRoute>
+              <Dashboard />
+            </PrivateAdminRoute>
+          }
+        />
+      </Routes>
+
+      <Route
+        path="products"
+        element={
+          <PrivateAdminRoute>
+            {" "}
+            <ProductsList />{" "}
+          </PrivateAdminRoute>
+        }
+      />
     </>
   );
 }
